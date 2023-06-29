4559a2f0f67a1ee64461392c58a97baa892c9ec4
hipolito <huaylla7@hotmail.com>
2023-06-28 21:39:38 -0400

    Creación del proyecto

---
+++ app/src/main/java/org/matematicasfacil/foodappy/MainActivity.java
@@ -0,0 +1,22 @@
+package org.matematicasfacil.foodappy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+import org.matematicasfacil.foodappy.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+    }
+}

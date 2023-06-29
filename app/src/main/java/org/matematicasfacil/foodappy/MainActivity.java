6d734e6c6787b900d4783cc282f2526630bdf8f5
hipolito <huaylla7@hotmail.com>
2023-06-28 22:16:00 -0400

    MainActivity funcionando.

--- app/src/main/java/org/matematicasfacil/foodappy/MainActivity.java
+++ app/src/main/java/org/matematicasfacil/foodappy/MainActivity.java
@@ -3,6 +3,8 @@ package org.matematicasfacil.foodappy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
@@ -13,7 +15,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.btnFood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent activityMenu = new Intent(getBaseContext(), MenuActivity.class);
+                startActivity(activityMenu);
+            }
+        });
 
     }
 }
